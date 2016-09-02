58236eed07a3d0a7c72500782b9775f708329cf6
bennycooly <ben_fu1@yahoo.com>
2016-09-01 23:29:13 -0500

    Initial commit

---
+++ Data Structures/Trees/BinaryTree/BinaryTree.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////
+/// BinaryTree.h
+/////////////////////////////////////////////////
+
+#pragma once
+
+#include <cstdint>
+
+template <typename T> class BinaryTree;
+
+template <typename T>
+class BinaryTree {
+public:
+    BinaryTree();
+    BinaryTree(T const&);
+    BinaryTree(BinaryTree const&);
+    ~BinaryTree();
+
+    void insert(T&);
+
+    void printTree();
+
+private:
+    class Node;
+
+    Node* root;
+
+    void destroy(Node*);
+};
+
+template <typename T>
+class BinaryTree<T>::Node {
+    T const& m_val;
+    Node* left;
+    Node* right;
+
+    Node();
+    Node(T const&);
+    Node(Node const&);
+    ~Node();
+};
+
---
+++ Data Structures/Trees/BinaryTree/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include "BinaryTree.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+    cout << "Trees" << endl;
+    BinaryTree<uint32_t> tree{ };
+}
